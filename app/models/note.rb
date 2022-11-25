d5171a6708a355b6b8af52e80214069e9a46a10d
KarthiS25 <karthis1015@gmail.com>
2022-11-25 20:13:25 +0530

    1st heroku

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,15 @@
 class Note < ApplicationRecord
   has_rich_text :content
   belongs_to :user
-  has_one_attached :image
+  #has_one_attached :image
+
+  #validates :image,  true
+  #validates :image, presence: true
+
+  before_create :console_check
+
+  def console_check  
+    puts "Successfully working before creation"
+  end
 
 end
